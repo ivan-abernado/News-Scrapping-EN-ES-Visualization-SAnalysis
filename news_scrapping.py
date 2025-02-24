b043668a74e8a3b3f64e38aa33470c7e60bb5598
Iván Alonso Bernadó <ivan.abernado@outlook.com>
2025-02-24 20:13:08 +0100

    Update news_scrapping.py
    added new functionalities and made the code less hard-coded

--- news_scrapping.py
+++ news_scrapping.py
@@ -1,15 +1,20 @@
 import newspaper
 import pandas as pd
 import re
+import os
 from tqdm import tqdm
 
 # This function scrapes whatever website we pass to it with the library newspaper
 # and saves all news into a .csv file
-def scrapping(web_domain: str) -> None:
-    # We especify which website we want to scrape
-    url = f'https://{web_domain}/'
+def scrapping(web_domain: str, language_code: str) -> None:
+    # We specify which website we want to scrape
+    url = f'https://{web_domain.lower()}'
 
-    website = newspaper.build(url)
+    # Now we overwrite the web_domain variable, so that the resulting csv
+    # will be saved with an apropiate name (e.g., 'nytimes.csv' instead of 'nytimes.com.csv'
+    web_domain = re.search(r'^(\w+)', web_domain).group()
+
+    website = newspaper.build(url, language=f'{language_code.lower()}')
 
     # List to which we will add all news.
     article_list = []
@@ -22,29 +27,52 @@ def scrapping(web_domain: str) -> None:
             article.download()
             article.parse()
 
-            # try-except function that handles None values or dates in other formats
-            try:
-                 # This regex pattern tries to match dates in formats similar to YYYY-MM-DD
-                date = re.search(r'(\d+)-(\d+)-(\d+)\b', str(article.publish_date)).group()
-            except AttributeError:
-                date = article.publish_date
+            # If article's title and text exist/are not missing values...
+            if article.title and article.text:
+                # try-except function that handles None values or dates in other formats
+                try:
+                     # This regex pattern tries to match dates in formats similar to YYYY-MM-DD
+                    date = re.search(r'(\d+)-(\d+)-(\d+)\b', str(article.publish_date)).group()
+                except AttributeError:
+                    date = article.publish_date
 
-            # Dictionary with the article's info that we will add to our previous list
-            article_data = {
-                'Title': article.title,
-                'Text': article.text,
-                'Date': date,
-                'Url': article.source_url
-            }
-            article_list.append(article_data)
+                # Dictionary with the article's info that we will add to our previous list
+                article_data = {
+                    'Source': web_domain.upper(),
+                    'Date': date,
+                    'Title': article.title,
+                    'Text': article.text
+                }
+                article_list.append(article_data)
+            else:
+                print('\nArticle skipped due to missing values (NaN)')
 
         except newspaper.article.ArticleException as e:
-            print(f'Error {e}, the scrapping will stop.')
+            print(f'Error {e}. The scrapping will stop. The already scrapped news will still be saved.')
             break
 
     pd.set_option('display.max_columns', None)
 
-    # We convert the list into a DataFrame and then we save it as a .csv file
+    # We convert the list into a DataFrame and drop any missing values (NaN),
+    # which will make it foolproof for the future
     df = pd.DataFrame(article_list)
+    df = df.dropna()
 
-    df.to_csv(f'{web_domain}.csv', mode='w', encoding='utf-8-sig', index=False)
+    # Ensure the 'CSVs' folder exists, create it if it doesn't
+    os.makedirs(f'News CSVs/{language_code.upper()}', exist_ok=True)
+
+    # Finally, we save the dataframe into a .csv file
+    df.to_csv(f'News CSVs/{language_code.upper()}/{web_domain.lower()}.csv', mode='w', encoding='utf-8-sig', index=False)
+
+
+# We specify from which newspaper we want to scrap news from
+# Note: Write the newspaper's domain
+news_website = 'elespanol.es'
+
+# We specify the language in which we want to scrap news in.
+# This serves two purposes:
+# -First, it allows us to scrap news from websites in multiple languages (for example, HuffPost)
+# -Second, it allows us to automatically save the final .csv file into its respective folder
+language = 'ES'
+
+scrapping(news_website, language)
